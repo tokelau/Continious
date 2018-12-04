9fb98ed3afc3a6ba23497c924e43afc17afe3b86
tokelau <lobckina.nastya@yandex.ru>
2018-12-04 19:18:25 +0800

    Update main.cpp

--- obj/main.cpp
+++ obj/main.cpp
@@ -6,7 +6,6 @@
 #pragma comment(lib,"pocketsphinx.lib")
 #pragma comment(lib,"sphinxbase.lib")
 
-
 #define MODELDIR "C:/Users/AvadaCedavra/Desktop/SpeechRec/test/Debug/model/zero_ru_cont_8k_v3"
 
 #if defined(_WIN32) && !defined(__CYGWIN__)
