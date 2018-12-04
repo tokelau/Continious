fe9d7917a4358989ce2c35761a761181760d50ae
tokelau <lobckina.nastya@yandex.ru>
2018-12-04 19:19:06 +0800

    Update main.cpp

--- obj/main.cpp
+++ obj/main.cpp
@@ -1,20 +1,8 @@
 #include <iostream>
-#include <fstream>
 #include <string>
 #include "recogniser.h"
 
-#pragma comment(lib,"pocketsphinx.lib")
-#pragma comment(lib,"sphinxbase.lib")
-
-#define MODELDIR "C:/Users/AvadaCedavra/Desktop/SpeechRec/test/Debug/model/zero_ru_cont_8k_v3"
-
-#if defined(_WIN32) && !defined(__CYGWIN__)
-#include <windows.h>
-#else
-#include <sys/select.h>
-#endif
-
-int main(int argc, char *argv[])
+int main()
 {
 	Recognizer rec;
 	rec.recognize_from_file();
